babe82a0705c73ea97a1b4f98c34262026695e09
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2020-11-30 14:31:58 -0500

    Outline skeleton of some methods
    I wrote out some of the methods we will need to code but i didnt think of all of them yet. I commented each of the methods so you know what it does.

--- src/com/company/ChatBot.java
+++ src/com/company/ChatBot.java
@@ -6,4 +6,30 @@ public class ChatBot {
     public ChatBot (String name){
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean containsMeanWords(String str) {
+        // Returns true if str contains a "mean" word
+
+    }
+
+    public boolean containsNiceWords(String str) {
+        // Returns true if str contains a "nice" word
+    }
+
+    public String getUserResponse() {
+        // Asks the user for a question or response and returns user's input
+
+    }
+
+    public int randomInt(int min, int max){
+        // returns a random integer from min to max
+    }
+
+    public String greet() {
+        // Returns a random string from the three pre-made greetings
+    }
 }
